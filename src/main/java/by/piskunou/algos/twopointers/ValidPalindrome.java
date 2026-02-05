541fca25c51cc94921f3b36531dde4936835cbcf
c.piskunou <t.piskunov@besmart.by>
2026-02-05 15:07:35 +0300

    valid palindrome

---
+++ src/main/java/by/piskunou/algos/twopointers/ValidPalindrome.java
@@ -0,0 +1,30 @@
+package by.piskunou.algos.twopointers;
+
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty() || s.length() == 1) {
+            return true;
+        }
+
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+
+            if (i > j) {
+                return true;
+            }
+
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+            i++; j--;
+        }
+        return true;
+    }
+}
