bf95020f8e3e4ba8e7c50af783b083b50648d8e6
c.piskunou <t.piskunov@besmart.by>
2026-03-04 14:16:42 +0300

    first steps in DailyTemperature

--- src/main/java/by/piskunou/algos/App.java
+++ src/main/java/by/piskunou/algos/App.java
@@ -1,42 +1,25 @@
 package by.piskunou.algos;
 
-import by.piskunou.algos.stack.minstack.MinStack;
-import by.piskunou.algos.stack.minstack.impl.MinStackImpl3;
-import by.piskunou.algos.stack.ValidParentheses;
+import by.piskunou.algos.stack.DailyTemperatures;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class App {
     public static void main(String[] args) {
         // --- Param(-s) ---
-        var param1 = "[]";
-        var param2 = "([{}])";
-        var param3 = "[({)";
-        var param4 = "()[";
-        var param5 = "]";
-        var params = List.of(param1, param2, param3, param4, param5);
+        var param1 = new int[]{30, 38, 30, 36, 35, 40, 28};
+        var param2 = new int[]{22,21,20};
+        var params = List.of(param1, param2);
 
         // --- Solution ---
-        var solution = new ValidParentheses();
+        var solution = new DailyTemperatures();
 
         // -- Answer(-s) ---
-        var ans = new ArrayList<Boolean>();
-        for (var i = 0; i < params.size(); i++) {
-            ans.add(solution.isValid(params.get(i)));
+        for (var param : params) {
+            var ans = Arrays.toString(solution.dailyTemperatures(param));
+            System.out.println(ans);
         }
-
-        // --- Print section ---
-        System.out.println(ans);
-
-        MinStack minStack = new MinStackImpl3();
-        minStack.push(1);
-        minStack.push(2);
-        minStack.push(0);
-        System.out.println(minStack.getMin());
-        minStack.pop();
-        System.out.println(minStack.top());
-        System.out.println(minStack.getMin());
     }
 }
---
+++ src/main/java/by/piskunou/algos/stack/DailyTemperatures.java
@@ -0,0 +1,7 @@
+package by.piskunou.algos.stack;
+
+public class DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        return new int[temperatures.length];
+    }
+}
